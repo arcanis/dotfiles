a87ae9fcc66907bf1c340eab9ce341d3abff8078
Maël Nison <nison.mael@gmail.com>
2013-06-17 11:33:35 +0200

    Adds installation script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+target="$1"
+if [ -z "$target" ]; then
+    echo "Usage: $0 <Installation path>"
+    exit 1
+else
+    target="$(realpath "$target")"
+    echo "Target is $target"
+fi
+
+( cd "$dir" ; git ls-files ) | cut -f1 -d/ | uniq \
+| while read file; do
+    if [ "$file" \!= "install.sh" ]; then
+        echo "Installing $file ..."
+        rm -rf "$target"/"$file"
+        ln -s "$dir"/"$file" "$target"/"$file"
+    fi
+done
